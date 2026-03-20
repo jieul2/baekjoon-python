aa37e11fa1327858bb2d4412c88a3aff1ceb1350
jieul2 <jieul2@kakao.com>
2026-03-20 22:39:53 +0900

    https://www.acmicpc.net/problem/34691

---
+++ Python/34691.py
@@ -0,0 +1,11 @@
+while True:
+    a = input()
+
+    if a == "animal":
+        print("Panthera tigris")
+    elif a == "tree":
+        print("Pinus densiflora")
+    elif a == "flower":
+        print("Forsythia koreana")
+    elif a == "end":
+        break
